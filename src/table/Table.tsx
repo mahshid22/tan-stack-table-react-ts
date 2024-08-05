9d247f4a082b8eae822d4099917afa17a64a3739
mahshid22 <mahshid.pouyafar@gmail.com>
2024-08-05 21:13:47 +0430

    add some css to table with tailwind

--- src/table/Table.tsx
+++ src/table/Table.tsx
@@ -19,12 +19,12 @@ export default function Table(props: any) {
     <div>
       <div className="c">TanStack Table</div>
       <div className="c">
-        <table>
+        <table  className="border-2 border-x-cyan-700">
           <thead>
             {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
+              <tr key={headerGroup.id} className="bg-slate-400">
                 {headerGroup.headers.map((header) => (
-                  <th key={header.id}>
+                  <th key={header.id} className="p-3 font-semibold border-2 border-x-cyan-700">
                     {header.isPlaceholder
                       ? null
                       : flexRender(
@@ -37,10 +37,10 @@ export default function Table(props: any) {
             ))}
           </thead>
           <tbody>
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id}>
+            {table.getRowModel().rows.map((row, index) => (
+              <tr key={row.id} className={`${index%2 ? 'bg-slate-300' : ''}`}>
                 {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id}>
+                  <td key={cell.id} className="p-3 border-2 border-x-cyan-700">
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </td>
                 ))}
